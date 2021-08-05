2662c095b038e06aa934adc516c3d473ef5ce51f
ithekozub <ithekozub@inbox.ru>
2021-08-06 01:07:34 +0300

    update 2

--- posts/templatetags/custom_filters.py
+++ posts/templatetags/custom_filters.py
@@ -7,3 +7,7 @@ register = template.Library()  # если мы не зарегистрируем
 @register.filter(name='get_item')
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
